d7118fbdb92deb828ecc1ee1cb28a9b645ae0120
Alex Harry <alexander.harry1@ecat.montana.edu>
2018-09-18 16:08:42 -0600

    pushing server.py

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 import socket;
-
+buffer_size = null
 #creating a server
 my_socket = socket.socket(socket.AF_INet, socket.SOCK_STREAM)
 my_socket.bind(('0.0.0.0', 5000))
 my_socket.listen(1)
 
-        #
+# createing the client side
+my_socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(('0.0.0.0',5000))
+my_socket.send(bytes('POST...'))
+data = my_socket.recv(buffer_size)
+my_socket.close()
+
+#server connect creates a new socket
+conn.addr = my_socket.accept()
+data = conn.recv(buffer_size)
+conn.send(data)
+conn.close()
