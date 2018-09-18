05f5859ba757abcd863464bf3858c9ad1b2b54f4
Alex Harry <alexander.harry1@ecat.montana.edu>
2018-09-18 17:35:03 -0600

    server.py close to done

--- server.py
+++ server.py
@@ -1,35 +1,35 @@
-import socket;
-#sets the buffer size
+import socket
+# sets the buffer size
 buffer_size = 1024
-#creating a server
+# creating a server
 my_socket = socket.socket(socket.AF_INet, socket.SOCK_STREAM)
-#binds to an address and port
+# binds to an address and port
 my_socket.bind(('0.0.0.0', 5000))
-#allows 1 connection
+# allows 1 connection
 my_socket.listen(1)
 
 # createing the client side
 my_socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connects to the ip and port
 my_socket.connect(('0.0.0.0',5000))
-#sends the bytes
+# sends the bytes
 my_socket.send(bytes('POST...'))
-#receives the data
+# receives the data
 data = my_socket.recv(buffer_size)
-#closes the connection
+# closes the connection
 my_socket.close()
 
-#creating a new socket
+# creating a new socket for the server
 connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#binding a new socket
+# binding a new socket
 connection.bind(('0.0.0.0', 5000))
-#setting amount of connections
+# setting amount of connections
 connection.listen(1)
-#server connect creates a new socket
+# server connect creates a new socket
 connection.addr = my_socket.accept()
-#receives the data
+# receives the data
 data = connection.recv(buffer_size)
-#sends the data
+# sends the data
 connection.send(data)
-#closes the connection
+# closes the connection
 connection.close()
