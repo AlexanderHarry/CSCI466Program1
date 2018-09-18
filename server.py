bf93e73337b814ff749b1e4fb6bd2a4aac3c15d9
Alex Harry <alexander.harry1@ecat.montana.edu>
2018-09-18 16:25:29 -0600

    More polished server.py file, with comments

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
 import socket;
-buffer_size = null
+#sets the buffer size
+buffer_size = 1024
 #creating a server
 my_socket = socket.socket(socket.AF_INet, socket.SOCK_STREAM)
+#binds to an address and port
 my_socket.bind(('0.0.0.0', 5000))
+#allows 1 connection
 my_socket.listen(1)
 
 # createing the client side
 my_socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connects to the ip and port
 my_socket.connect(('0.0.0.0',5000))
+#sends the bytes
 my_socket.send(bytes('POST...'))
+#receives the data
 data = my_socket.recv(buffer_size)
+#closes the connection
 my_socket.close()
 
+#creating a new socket
+connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#binding a new socket
+connection.bind(('0.0.0.0', 5000))
+#setting amount of connections
+connection.listen(1)
 #server connect creates a new socket
-conn.addr = my_socket.accept()
-data = conn.recv(buffer_size)
-conn.send(data)
-conn.close()
+connection.addr = my_socket.accept()
+#receives the data
+data = connection.recv(buffer_size)
+#sends the data
+connection.send(data)
+#closes the connection
+connection.close()
