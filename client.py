22cd11590712cea684cf16012a371f3842fefc25
Justin Keeling <jrkeeling23@gmail.com>
2018-09-20 20:43:39 -0600

    run server, go to url, type http://127.0.0.1, should say yes.

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+# creating the client side
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connects to the ip and port
+s.connect(('127.0.0.1', 80))
+# sends the bytes
+s.send(bytes('POST...'))
+# receives the data
+data = s.recv(1024)
+# closes the connection
+s.close()
--- server.py
+++ server.py
@@ -1,35 +1,17 @@
-import socket;
-#sets the buffer size
+import socket
+# sets the buffer size
 buffer_size = 1024
-#creating a server
-my_socket = socket.socket(socket.AF_INet, socket.SOCK_STREAM)
-#binds to an address and port
-my_socket.bind(('0.0.0.0', 5000))
-#allows 1 connection
-my_socket.listen(1)
+# creating a server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# binds to an address and port
+s.bind(('127.0.0.1', 80))
+# allows 1 connection
+s.listen(1)
 
-# createing the client side
-my_socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connects to the ip and port
-my_socket.connect(('0.0.0.0',5000))
-#sends the bytes
-my_socket.send(bytes('POST...'))
-#receives the data
-data = my_socket.recv(buffer_size)
-#closes the connection
-my_socket.close()
+conn, addr = s.accept()
 
-#creating a new socket
-connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#binding a new socket
-connection.bind(('0.0.0.0', 5000))
-#setting amount of connections
-connection.listen(1)
-#server connect creates a new socket
-connection.addr = my_socket.accept()
-#receives the data
-data = connection.recv(buffer_size)
-#sends the data
-connection.send(data)
-#closes the connection
-connection.close()
+data = conn.recv(buffer_size)
+print("Received request: '" + data.decode('utf-8')+"'")
+
+conn.send('Yes'.encode('utf-8'))
+conn.close()
