90c028e402e4b9232957647408b725594f25a150
AlexHarry17 <alex.harry1@gmail.com>
2018-09-22 13:27:00 -0600

    reverting back to start from yesterday.  changed water to _

--- board_manager.py
+++ board_manager.py
@@ -1,187 +1,62 @@
 import sys
-import random
 
 length, width = 12, 11
-coordinate_letters = 'ABCDEFGHIJ'
+side = 'ABCDEFGHIJ'
 matrix = [[0 for x in range(width)] for y in range(length)]
-
-
-def get_ship_length(self, ship):
-    if ship == "Carrier":
-        return 5
-    elif ship == "Battleship":
-        return 4
-    elif ship == "Cruiser" or ship == "Submarine":
-        return 3
-    elif ship == "Destroyer":
-        return 2
-    else:
-        print("Bad ship given, no value")
 
 
 class BoardManager(object):
 
-    def setup_ships(self, ship_list, ship_direction, ship_right_left_down_up):
-        while not is_empty(ship_list):
-            ship_selected = ship_list.pop(random.randint(0, len(ship_list) - 1))  # pops a random ship to insert
-            # selects a random ship direction
-            ship_direction_selected = random.choice(ship_direction)
-        # selects randomly what way a ship will go from its starting location
-        ship_right_left_down_up_selection = random.choice(ship_right_left_down_up)
-        rand_x = random.randint(1, 11)  # sets a random x in matrix
-        rand_y = random.randint(1, 11)  # sets a random y in matrix
-        # runs if statement if the ship will fit
-        if does_ship_fit(ship_selected, ship_direction_selected, ship_right_left_down_up_selection, rand_x, rand_y):
-            # inserts ship into the board
-            insert_ships(ship_selected, ship_direction_selected, ship_right_left_down_up_selection, rand_x,
-                         rand_y)
-        else:
-            ship_list.append(ship_selected)  # appends the ship that didn't fit back into the list
-
-
-pass
-
-
-# Method to check if the player has inserted all of their ships onto the board
-def is_empty(ship_list):
-    if ship_list:
-        return False
-    else:
-        return True
-
-
-pass
-
-
-# Method to check if the ship will fit in the board
-def does_ship_fit(self, ship, direction, left_right_up_down, location_x, location_y):
-    ship_length = get_ship_length(self, ship)  # gets the length of the ship that is trying to get inserted
-    if direction == "Horizontal":  # Checks if the ship is going to be inserted horizontally
-        # checks to see if the ship is being inserted to the right of the
-        # initial location
-        if left_right_up_down == "Right":
-
-            if (
-                    # checks that the ship falls in
-                    # the board and will fit
-                    width - 1) - location_x >= ship_length and location_x != 0:
-                # loops to make sure the ship wont interfere with a ship already on the
-                for i in range(location_x, width):
-                    # board
-                    if matrix[i][location_y] != "_":
-                        return False  # returns a false that the ship cannot be inserted
+    def start_board(self):
+        for i in range(0, width - 1):
+            for j in range(0, length - 1):
+                if j == 0 and i > 0:
+                    matrix[i][j] = side[i]
+                elif i == 0 and j > 0:
+                    matrix[i][j] = j
+                elif i == 0 and j == 0:
+                    matrix[i][j] = ' '
                 else:
-                    return True  # returns true if the ship can be inserted
-            else:
-                return False  # returns a false that the ship cannot be inserted
-        if left_right_up_down == "Left":  # checks that the shift is being placed to the left of the initial peg
-            # checks that the ship falls in the board and
-            # will fit
-            if location_x - 1 >= ship_length and location_x != 0:
-                for i in reversed(
-                        range(location_x, 0)):  # checks to make sure the ship will fit.  loops in i-- order
-                    if matrix[i][location_y] != "_" or i == 0:  # makes sure a current ship isn't floating here
-                        return False  # a ship cannot be inserted here
+                    matrix[i][j] = '_'
+                pass
+
+    def print_board(self):
+        sys.stdout = open('own_board.txt', 'w')
+        for i in range(0, width - 1):
+            for j in range(0, length - 1):
+                print(matrix[i][j], end=" ")
+            print()
+
+    pass
+
+    def convert_user_input(x, y):
+        if x.isalpha():
+            found = False
+            while not found:
+                for i in side:
+                    if x == side[i]:
+                        new_x = i
+                        found = True
                     else:
-                        return True  # a ship can be inserted
-            else:
-                return False  # a ship cannot be inserted
-    if direction == "Vertical":
-        if left_right_up_down == "Up":  # checks if the ship is being located up from the initial matrix location
-            if location_y != 0 and (
-                    # checks that the ship doesn't fall on the unallowed boarder
-                    #  and the the initial length is OK
-                    location_y - 1) >= ship_length:
-                for i in reversed(range(location_y, 0)):  # loops from initial array location in i-- order
-                    if matrix[location_x][i] != "_" or i == 0:  # makes sure a ship is not here
-                        return False  # ship is not allowed to be inserted
-                    else:
-                        return True  # a ship is allowed here
-            else:
-                return False  # a ship is not allowed here
+                        found = False
+        else:
+            print('coordinate X invalid')
+        if not y.isint():
+            print('coordinate Y invalid')
+        else:
+            hit = check_if_hit(new_x, y)
+        return hit
 
-            if left_right_up_down == "Down":  # checks for a ship to be inserted down from the initial matrix
-                # location
-                if location_y != 0 and (
-                        location_y - 1) >= ship_length:  # checks to make sure the ship will fit in this location
-                    #  and that it doesn't fall on the boarder
-                    for i in range(location_y, 0):  # loops in to see if a ship is already here
-                        if matrix[location_x][i] != "_" or i == 0:
-                            return False  # ship cannot be inserted
-                return True  # a ship can be inserted
-            else:
-                return False  # a ship cannot be inserted
+    pass
 
 
-pass
-
-
-def insert_ships(self, ship, direction, left_right_up_down, location_x, location_y):
-    # checks if insertion is for horizontal
-    if direction == "horizontal":
-        direction_for_ship = 1
-        ship_length = get_ship_length(self, ship)
-
-
-pass
-
-
-def start_board(self):  # This method fills an array to create a clear game board.
-    for i in range(0, width - 1):
-        for j in range(0, length - 1):
-            if j == 0 and i > 0:  # If left side of the board , then put in coordinate system.
-                matrix[i][j] = coordinate_letters[i]
-            elif i == 0 and j > 0:  # If top of the board, then put in coordinate system.
-                matrix[i][j] = j
-            elif i == 0 and j == 0:  # If top left corner, then put a space.
-                matrix[i][j] = ' '
-            else:  # Playing field
-                matrix[i][j] = '~'
-
-
-pass
-
-
-def print_board(self):  # Prints the board into a file
-    sys.stdout = open('own_board.txt', 'w')
-    for i in range(0, width - 1):
-        for j in range(0, length - 1):
-            print(matrix[i][j], end=" ")
-        print()
-
-
-pass
-
-
-def convert_user_input(x, y):  # Converts the users inputs into operable matrix inputs
-    if x.isalpha():  # tests if first input is a character.
-        found = False
-        while not found:  # validation for input. NOTE!!! may not need, could cause infinite loop too
-            for i in coordinate_letters:  # Iterates through coordinate string to find correct value for matrix.
-                if x == coordinate_letters[i]:  # May need to adjust parameters .
-                    new_x = i
-                    found = True
-    else:
-        print('coordinate X invalid')
-    if not y.isint():
-        print('coordinate Y invalid')
-    else:
-        hit = check_if_hit(new_x, y)
-    return hit
-
-
-pass
-
-
-def check_if_hit(x, y):  # Checks with with values in matrix to change board.
-    if matrix[x][y] == 'C' or matrix[x][y] == 'B' or matrix[x][y] == 'R' or matrix[x][y] == 'S' or \
-            matrix[x][y] == 'D' \
-            or matrix[x][y] == 'D':  # checks what kind of ship is hit.
-        matrix[x][y] = 'X'  # X is for hit on game board.
+def check_if_hit(x, y):
+    if matrix[x][y] == 'C' or matrix[x][y] == 'B' or matrix[x][y] == 'R' or matrix[x][y] == 'S' or matrix[x][y] == 'D' \
+            or matrix[x][y] == 'D':
+        matrix[x][y] = 'X'
         return True
     else:
-        matrix[x][y] = '0'  # 0 is miss on game board.
+        matrix[x][y] = '0'
         return False
-
 
 pass
