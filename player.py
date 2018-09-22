32fbb25baa48ccb0f68d5772a82d3debc6792bc2
AlexHarry17 <alex.harry1@gmail.com>
2018-09-21 18:38:33 -0600

    added player.py

---
+++ player.py
@@ -0,0 +1,20 @@
+from board_manager import BoardManager
+# from server import Server
+
+
+class Player():
+  #  playerServer = Server()
+    board = BoardManager()
+    board.start_board()
+    board.print_board()
+
+
+
+
+
+
+
+
+
+
+
